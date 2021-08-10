86eff534c894e64e98265fff849336465745d812
EliasRDS <elias.ctx3@gmail.com>
2021-08-10 17:27:41 -0300

    First commit

---
+++ src/com/papai/PrimeiroPrograma.java
@@ -0,0 +1,28 @@
+package com.papai;
+
+import br.com.papai.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato (null, null, null);	
+		Livros Livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(Livros);
+		
+		
+	/*int a = 2;
+		int b = 3;
+		System.out.println("Hello my Lord" + (a+b));*/
+		
+	}
+	
+	
+	class Livros {
+	 private String nome;
+	 private String npag;
+	 
+
+}
